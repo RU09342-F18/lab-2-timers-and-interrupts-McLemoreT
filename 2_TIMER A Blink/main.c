814202c688e86b2690d8db1f84efc1dd0bc1a6ae
unknown <mclemoret0@students.rowan.edu>
2018-09-28 19:28:11 -0400

    Used different interupt

--- 2_TIMER A Blink/main.c
+++ 2_TIMER A Blink/main.c
@@ -17,6 +17,7 @@ int main(void)
 {
 
     WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
+
     P1DIR |= LED;               //Set Port 1.0 as an output
     P1OUT &= ~LED;              //Set the initial value of port 1.0 as "0"
 
@@ -24,13 +25,15 @@ int main(void)
 
     TimerSetup(10);
     __enable_interrupt();
-    __bis_SR_register(LPM4_bits + GIE);
+    __bis_SR_register(LPM0 + GIE); // LPM0 with interrupts enabled
+   // __bis_SR_register(LPM4_bits + GIE);
 }
 void TimerSetup(int rate){
     CCTL0 = CCIE;
-    TA0CCTL0 = CCIE; // Enable interrupt in compare mode
+    //TA0CCTL0 = CCIE; // Enable interrupt in compare mode
     TA0CTL = TASSEL_2 + MC_1 + ID_2; // SMCLK/4, Up
-    TA0CCR0 = 250000 / rate; // 250000 / 10 = 25000, (10^6 [Hz] / 4) / (25000) = 10Hz
+    //TACTL = TASSEL_2 + MC_1 + ID_3; // SMCLK/8, UPMODE
+    TA0CCR0 = 125000 / rate; // 250000 / 10 = 25000, (10^6 [Hz] / 4) / (25000) = 10Hz
 }
 //#pragma vector=PORT1_VECTOR
 //__interrupt void Port_1(void)
