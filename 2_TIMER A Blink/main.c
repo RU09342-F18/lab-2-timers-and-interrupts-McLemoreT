860c7b32f689e23e3a9902747c6fdf17f3f58243
unknown <mclemoret0@students.rowan.edu>
2018-09-28 19:18:58 -0400

    Removed old button code

--- 2_TIMER A Blink/main.c
+++ 2_TIMER A Blink/main.c
@@ -20,17 +20,9 @@ int main(void)
     P1DIR |= LED;               //Set Port 1.0 as an output
     P1OUT &= ~LED;              //Set the initial value of port 1.0 as "0"
 
-    P1DIR &= ~Btn;              //Set Port 1.3 as an input
-    P1REN |= Btn;               //Turn on the pull-up resistor for port 1.3
-    P1OUT |= Btn;               //Set the initial value of port 1.3 as "1"
-
-    P1IE |=  BIT3;                            // P1.3 interrupt enabled
-    P1IES |= BIT3;                            //falling edge
-    P1REN |= BIT3;                            // Enable resistor on SW2 (P1.3)
-    P1OUT |= BIT3;                             //Pull up resistor on P1.3
-    P1IFG &= ~BIT3; // P1.3 Interrupt Flag cleared
 
 
+    TimerSetup(10);
     __enable_interrupt();
     __bis_SR_register(LPM4_bits + GIE);
 }
@@ -38,7 +30,7 @@ void TimerSetup(int rate){
     CCTL0 = CCIE;
     TA0CCTL0 = CCIE; // Enable interrupt in compare mode
     TA0CTL = TASSEL_2 + MC_1 + ID_2; // SMCLK/4, Up
-    TA0CCR0 = 250000 / 5; // 250000 / 10 = 25000, (10^6 [Hz] / 4) / (25000) = 10Hz
+    TA0CCR0 = 250000 / rate; // 250000 / 10 = 25000, (10^6 [Hz] / 4) / (25000) = 10Hz
 }
 //#pragma vector=PORT1_VECTOR
 //__interrupt void Port_1(void)
